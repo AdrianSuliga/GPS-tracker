5c74591b39391b428d503d7649e8761025d47c2e
Adrian Suliga <adriansuliga81@gmail.com>
2026-04-13 20:21:47 +0200

    [nRF9151DK] Change project to be multi-file

--- nRF9151DK/src/main.c
+++ nRF9151DK/src/main.c
@@ -1,62 +1,9 @@
-#include <zephyr/kernel.h>
+#include "lte_common.h"
+
 #include <zephyr/logging/log.h>
-#include <modem/nrf_modem_lib.h>
-#include <modem/lte_lc.h>
 #include <dk_buttons_and_leds.h>
 
-LOG_MODULE_REGISTER(GPS_Tracker, LOG_LEVEL_INF);
-
-static K_SEM_DEFINE(lte_connected, 0, 1);
-
-static void lte_handler(const struct lte_lc_evt *const evt)
-{
-    switch (evt->type) {
-        case LTE_LC_EVT_NW_REG_STATUS:
-            if (
-                (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_HOME) &&
-                (evt->nw_reg_status != LTE_LC_NW_REG_REGISTERED_ROAMING)
-            ) {
-                break;
-            }
-
-            k_sem_give(&lte_connected);
-            dk_set_led_on(DK_LED2);
-
-            break;
-
-        case LTE_LC_EVT_RRC_UPDATE:
-            LOG_INF("RRC mode: %s", evt->rrc_mode == LTE_LC_RRC_MODE_CONNECTED ?
-                    "Connected" : "Idle");
-            break;
-
-        default:
-            break;
-    }
-}
-
-static int modem_configure()
-{
-    int err;
-    
-    LOG_INF("Initializing modem library");
-
-    err = nrf_modem_lib_init();
-    if (err) {
-        LOG_ERR("Failed to initialize the modem library, error %d", err);
-        return err;
-    }
-
-    LOG_INF("Modem library initialized");
-    LOG_INF("Connecting to LTE network");
-
-    err = lte_lc_connect_async(lte_handler);
-    if (err) {
-        LOG_ERR("Error when calling lte_lc_connect_async, error %d", err);
-        return err;
-    }
-
-    return 0;
-}
+LOG_MODULE_REGISTER(GPS_Main, LOG_LEVEL_INF);
 
 int main()
 {
@@ -68,12 +15,14 @@ int main()
         return err;
     }
 
+    // Initialize modem and connect to LTE network
     err = modem_configure();
     if (err) {
         LOG_ERR("Failed to configure modem");
         return err;
     }
 
+    // Wait for LTE connection
     k_sem_take(&lte_connected, K_FOREVER);
 
     LOG_INF("Connected to LTE network");
